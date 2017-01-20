e411d57f20763c9a45cf1fc9e9125c9f51e3ecee
Anwar Nunez-Elizalde <anwarnunez@gmail.com>
2017-01-19 17:13:01 -0800

    ENH: import baseprior

--- tikypy/temporal_priors.py
+++ tikypy/temporal_priors.py
@@ -1,6 +1,7 @@
 import numpy as np
 import itertools
 
+from tikypy import BasePrior
 import tikypy.utils as tikutils
 
 
@@ -17,49 +18,6 @@ def get_delays_from_prior(raw_prior, delays):
         delays = np.asarray(delays)
         prior = tikutils.fast_indexing(raw_prior, delays, delays)
     return prior, delays
-
-
-
-##############################
-# Classes
-##############################
-
-class BasePrior(object):
-    '''Base class for priors
-    '''
-    def __init__(self, prior, dodetnorm=False):
-        '''
-        '''
-        assert prior.ndim == 2
-
-        self.prior = prior.copy()
-        self.detnorm = 1.0
-        self.penalty = None
-        self.penalty_detnorm = 1.0
-        self.dodetnorm = dodetnorm
-
-        if self.dodetnorm:
-            self.normalize_prior()
-
-    @property
-    def asarray(self):
-        return self.prior
-
-    def prior2penalty(self, regularizer=0.0, dodetnorm=False):
-        penalty = np.linalg.inv(self.prior + regularizer*np.eye(self.prior.shape[0]))
-        if self.penalty is None:
-            # first time
-            self.penalty = penalty
-        detnorm = tikutils.determinant_normalizer(penalty) if dodetnorm else 1.0
-        return penalty / detnorm
-
-    def normalize_prior(self):
-        self.detnorm = tikutils.determinant_normalizer(self.prior)
-        self.prior /= self.detnorm
-
-    def normalize_penalty(self):
-        self.penalty_detnorm = tikutils.determinant_normalizer(self.penalty)
-        self.penalty /= self.penalty_detnorm
 
 
 class TemporalPrior(BasePrior):
