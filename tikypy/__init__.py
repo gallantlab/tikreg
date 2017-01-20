b7c7e0163b4f8e78dcfca812631b836c9fdb7528
Anwar Nunez-Elizalde <anwarnunez@gmail.com>
2017-01-19 17:12:23 -0800

    EHH: move baseprior

---
+++ tikypy/__init__.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tikypy.utils as tikutils
+
+
+##############################
+# Classes
+##############################
+
+class BasePrior(object):
+    '''Base class for priors
+    '''
+    def __init__(self, prior, dodetnorm=False):
+        '''
+        '''
+        assert prior.ndim == 2
+
+        self.prior = prior.copy()
+        self.detnorm = 1.0
+        self.penalty = None
+        self.penalty_detnorm = 1.0
+        self.dodetnorm = dodetnorm
+
+        if self.dodetnorm:
+            self.normalize_prior()
+
+    @property
+    def asarray(self):
+        return self.prior
+
+    def prior2penalty(self, regularizer=0.0, dodetnorm=False):
+        penalty = np.linalg.inv(self.prior + regularizer*np.eye(self.prior.shape[0]))
+        if self.penalty is None:
+            # first time
+            self.penalty = penalty
+        detnorm = tikutils.determinant_normalizer(penalty) if dodetnorm else 1.0
+        return penalty / detnorm
+
+    def normalize_prior(self):
+        self.detnorm = tikutils.determinant_normalizer(self.prior)
+        self.prior /= self.detnorm
+
+    def normalize_penalty(self):
+        self.penalty_detnorm = tikutils.determinant_normalizer(self.penalty)
+        self.penalty /= self.penalty_detnorm
