da82846c9370eeedd9803dc56995922b4e3f004d
Anwar Nunez-Elizalde <anwarnunez@gmail.com>
2017-01-20 02:23:04 -0800

    TEST: API solve OLS

--- tikypy/tests/test_api.py
+++ tikypy/tests/test_api.py
@@ -1,8 +1,13 @@
 import numpy as np
 import tikypy as tk
-from tikypy import models, spatial_priors as sps, temporal_priors as tps
+from tikypy import (models,
+                    spatial_priors as sps,
+                    temporal_priors as tps,
+                    utils as tikutils,
+                    )
 
-def test_band_api():
+def test_mkl_ols():
+    ndelays=1
     Af = np.random.randn(150, 10)
     Bf = np.random.randn(150, 20)
     Cf = np.random.randn(150, 30)
@@ -23,43 +28,69 @@ def test_band_api():
     features_train = [A,B,C]
     features_test = [Atest, Btest, Ctest]
 
+    delays = range(ndelays)
+
+    print('')
+    direct_fit = models.solve_l2_primal(tikutils.delay_signal(np.hstack(features_train), delays),
+                                        Ytrain,
+                                        tikutils.delay_signal(np.hstack(features_test), delays),
+                                        Ytest,
+                                        verbose=True,
+                                        weights=True, performance=True)
+
     spatial_priors = [sps.SphericalPrior(),
                       sps.SphericalPrior(),
                       sps.SphericalPrior(),
                       ]
 
 
-    delays = range(10)
+
     wishart_prior = tps.SphericalPrior(delays)
-    temporal_prior = tps.SmoothnessPrior(delays).set_wishart(wishart_prior)
-    temporal_prior = tps.GaussianKernelPrior(delays)
-    temporal_prior = tps.HRFPrior(delays)
-    temporal_prior = tps.SphericalPrior(delays)
 
-    Ktrain = 0.
-    Ktest = 0.
+    tpriors = [tps.SmoothnessPrior(delays).set_wishart(wishart_prior),
+               tps.GaussianKernelPrior(delays),
+               tps.HRFPrior([1] if delays == [0] else delays),
+               tps.SphericalPrior(delays),
+               ]
 
 
-    for fs_train, fs_test, feature_prior in zip(features_train,
-                                                features_test,
-                                                spatial_priors):
-        feature_prior.update_prior(fs_train.shape[-1])
-        if hasattr(temporal_prior, 'update_prior'):
-            temporal_prior.update_prior()
+    for temporal_prior in tpriors:
+        print(temporal_prior)
+        Ktrain = 0.
+        Ktest = 0.
 
-        kernel_train = models.kernel_spatiotemporal_prior(fs_train,
-                                                          temporal_prior.asarray,
-                                                          feature_prior.asarray)
-        Ktrain += kernel_train
+        for fs_train, fs_test, feature_prior in zip(features_train,
+                                                    features_test,
+                                                    spatial_priors):
+            feature_prior.update_prior(fs_train.shape[-1])
+            if hasattr(temporal_prior, 'update_prior'):
+                temporal_prior.update_prior()
 
-        kernel_test = models.kernel_spatiotemporal_prior(fs_train,
-                                                         temporal_prior.asarray,
-                                                         feature_prior.asarray,
-                                                         Xtest=fs_test)
-        Ktest += kernel_test
+            kernel_train = models.kernel_spatiotemporal_prior(fs_train,
+                                                              temporal_prior.asarray,
+                                                              feature_prior.asarray,
+                                                              delays=delays)
+            Ktrain += kernel_train
 
-    fit = models.solve_l2_dual(Ktrain, Ytrain,
-                               Ktest, Ytest,
-                               ridges=[0., 1., 10.0, 100.],
-                               verbose=True,
-                               weights=True, performance=True)
+            kernel_test = models.kernel_spatiotemporal_prior(fs_train,
+                                                             temporal_prior.asarray,
+                                                             feature_prior.asarray,
+                                                             Xtest=fs_test,
+                                                             delays=delays)
+            Ktest += kernel_test
+
+        fit = models.solve_l2_dual(Ktrain, Ytrain,
+                                   Ktest, Ytest,
+                                   ridges=[0., 1e-03, 1., 10.0, 100.],
+                                   verbose=True,
+                                   weights=True, performance=True)
+
+
+        assert np.allclose(fit['performance'][0], 1.)
+
+        weights = np.tensordot(tikutils.delay_signal(np.hstack(features_train), delays).T,
+                               fit['weights'], (1,1)).swapaxes(0,1)
+        if not np.allclose(temporal_prior.asarray, 1):
+            # scale weights
+            weights *= temporal_prior.asarray
+        assert np.allclose(weights[0], direct_fit['weights'])
